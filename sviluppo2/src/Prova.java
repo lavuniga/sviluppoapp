2c7b9f5f0c1e996f80d09db97a962d17542e134b
st116618 <st116618@dijkstra27.labinfo.educ.di.unito.it>
2014-04-30 12:24:23 +0200

    aaa

---
+++ sviluppo2/src/Prova.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author st116618
+ */
+public class Prova {
+public static void main(String args[]){
+System.out.println("ciao");
+}
+}
